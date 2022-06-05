cef8df753be88dbfb4aeae43e0df42d4b732c3c0
KawaiLi <kawai.li@alum.udep.edu.pe>
2022-06-05 03:28:47 -0500

    Formulario con link

--- arrays.php
+++ arrays.php
@@ -1,6 +1,16 @@
+<!DOCTYPE html>
+<html>
+<html lang="en">
+  <title>TRABAJO SIN_2022I</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+
 <?php
-include 'header.php';
-echo $header_hmtl;
 $libros = array('McGrawHill' => array('Tema' => 'Energía', 'Año' => '1995'),
                 'Pearson' => array('Tema' => 'Cálculo', 'Año' => '2003'),
                 'Internacional' => array('Tema' => 'Filosofía', 'Año' =>'2012' ),
@@ -18,40 +28,37 @@ $libros = array('McGrawHill' => array('Tema' => 'Energía', 'Año' => '1995'),
   <body>
 
     <?php
-    if(array_key_exists('brand_label', $_REQUEST) && array_key_exists($_REQUEST('brand_data'),$libros)) {
-      $brand_data = $libros($_REQUEST('brand_label'));
+    if(array_key_exists('brand_label', $_REQUEST) && array_key_exists($_REQUEST['brand_label'],$libros)) {
+      $brand_data = $libros[$_REQUEST['brand_label']];
       echo '<tr>';
         echo '<td>';
-      echo $REQUEST('brand_label');
+        echo '<a href = "arrays.php?brand_label ='.$REQUEST['brand_label'].'">'.$REQUEST['brand_label'].'</a>';
         echo '</td>';
         echo '<td>';
-      echo $brand_data('Tema');
+      echo $brand_data['Tema'];
         echo '</td>';
         echo '<td>';
-      echo $brand_data('Año');
+      echo $brand_data['Año'];
         echo '</td>';
       echo '</tr>';
-      else {
-        foreach($libros as $brand_label => $brand_data){
+    } else {
+        foreach($libros as $brand_label => $brand_data) {
           echo '<tr>';
             echo '<td>';
-          echo $REQUEST('brand_label');
+            echo '<a href = "arrays.php?brand_label ='.$brand_label.'">'.$brand_label.'</a>';
             echo '</td>';
             echo '<td>';
-          echo $brand_data('Tema');
+          echo $brand_data['Tema'];
             echo '</td>';
             echo '<td>';
-          echo $brand_data('Año');
+          echo $brand_data['Año'];
             echo '</td>';
           echo '</tr>';
         }
       }
 
-    }
       ?>
     </body>
   </table>
-
-  <?php
-  echo $footer_hmtl;
-    ?>
+</body>
+</html>
---
+++ headerpost.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<html lang="en">
+  <title>TRABAJO SIN_2022I</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+ <form action = 'header.php' method 'post'>
+   <input type 'text' name = 'brand_label'></input>
+   <input type 'submit'></input>
+ </form>
+
+</body>
+</html>
